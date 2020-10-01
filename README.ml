99343f07bdb731387b72ce22e78c8ef15c4154ad
mhk119 <58151072+mhk119@users.noreply.github.com>
2020-10-01 11:05:16 -0400

    Update README.ml

--- README.ml
+++ README.ml
@@ -1,3 +1,12 @@
 # 144 is the largest square in the Fibonacci Sequence
 
 A formalisation of Cohn's Proof.
+
+The files in src are in the following order:
+1. recursions
+2. gcd
+3. divisibility1
+4. divisibility2
+5. luc_squares
+6. luc_double_squares
+7. fib_squares
